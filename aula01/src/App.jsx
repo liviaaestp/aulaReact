3dab98637341d6c4bb277c2e14391cf4524d271f
liviaaestp <liviaamandaestevaodepaula@gmail.com>
2026-04-14 11:38:24 -0300

    14/04

---
+++ aula01/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App ()
+{
+
+  return (
+    <div>
+      
+      <h1>Hello Word !!! </h1>
+
+      <p>
+        Olá, seja bem vindo ao React com JS.
+      </p>
+
+    </div>
+  )
+}
