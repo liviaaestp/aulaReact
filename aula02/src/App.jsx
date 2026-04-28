820890a75903eccb5ee0560eb24b5964c8bca3b4
liviaaestp <liviaamandaestevaodepaula@gmail.com>
2026-04-28 11:35:29 -0300

    28/04

---
+++ aula02/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Exemplo1 from "./Exemplo1";
+export default function App()
+{
+  return (
+    <div>
+      <h1> Aula 02 - Criação de componentes</h1>
+
+     <div className="card">
+      <h3>Chamadas para o componente Exeplo1</h3>
+
+      <Exemplo1 numero1={10} numero2={20} />
+     </div>
+
+    </div>
+  )
+}
