ec1f8bd02dcdfa2020fbc69a7fee52f955e6409c
liviaaestp <liviaamandaestevaodepaula@gmail.com>
2026-05-26 11:39:57 -0300

    26/05

--- aula02/src/exercicio6.jsx
+++ aula02/src/exercicio6.jsx
@@ -8,7 +8,6 @@ export default function Exercicio6({ capital, taxa, tempo }) {
   
     return (
       <div style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '8px', margin: '10px 0' }}>
-        <h3>Exercício 6 </h3>
         <p><strong>Capital Inicial:</strong> R$ {capital.toFixed(2)}</p>
         <p><strong>Taxa de Juros:</strong> {taxa}% ao mês</p>
         <p><strong>Tempo:</strong> {tempo} meses</p>
